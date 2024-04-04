19d8262e460ec1ce184c321d7b7b41ec0bf3b02b
lrsanttos <lrsanttos@gmail.com>
2024-04-04 19:47:57 -0300

    Add novo produto;

--- src/Relatorio.java
+++ src/Relatorio.java
@@ -1,6 +1,7 @@
 public class Relatorio {
     public static void main(String[] args) {
         Produto produto1 = new Produto("Coca-cola", 5.50f);
+        Produto produto2 = new Produto("Pepsi", 3.50f);
         Pedido pedido1 = new Pedido(123);
 
 
